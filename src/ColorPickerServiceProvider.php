941e909e32b63f070c02bb7927cfb5a44bb27fb9
Shane Barron <shane@Mac.hsd1.fl.comcast.net>
2025-12-15 18:51:42 -0500

    Fix PHP class naming (PascalCase)

--- src/ColorPickerServiceProvider.php
+++ src/ColorPickerServiceProvider.php
@@ -3,14 +3,35 @@
 namespace MrShaneBarron\ColorPicker;
 
 use Illuminate\Support\ServiceProvider;
+use MrShaneBarron\ColorPicker\Livewire\ColorPicker;
+use MrShaneBarron\ColorPicker\View\Components\color-picker as BladeColorPicker;
+use Livewire\Livewire;
 
 class ColorPickerServiceProvider extends ServiceProvider
 {
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/sb-color-picker.php', 'sb-color-picker');
+    }
+
     public function boot(): void
     {
-        if (class_exists(\Livewire\Livewire::class)) {
-            \Livewire\Livewire::component('sb-color-picker', Livewire\ColorPicker::class);
-        }
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'sb-color-picker');
+
+        Livewire::component('sb-color-picker', color-picker::class);
+
+        $this->loadViewComponentsAs('ld', [
+            BladeColorPicker::class,
+        ]);
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/sb-color-picker.php' => config_path('sb-color-picker.php'),
+            ], 'sb-color-picker-config');
+
+            $this->publishes([
+                __DIR__ . '/../resources/views' => resource_path('views/vendor/sb-color-picker'),
+            ], 'sb-color-picker-views');
+        }
     }
 }
